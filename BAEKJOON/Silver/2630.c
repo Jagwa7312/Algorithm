185c05f7766036e086e97be18e6b099f78c432b4
Jagwa7312 <sat7312@gmail.com>
2022-01-19 16:33:35 +0900

    2630

---
+++ BAEKJOON/Silver/2630.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int result[2], grid[130][130];
+
+bool check(int row, int col, int num) {
+    int start = grid[row][col];
+
+    for (int i = row; i < row + num; i++) {
+        for (int j = col; j < col + num; j++) {
+            if (start != grid[i][j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void divide(int row, int col, int num) {
+    if (check(row, col, num)) {
+        result[grid[row][col]]++;
+    }
+
+    else {
+        int size = num / 2;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                divide(row + size * i, col + size * j, size);
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &grid[i][j]);
+        }
+    }
+
+    divide(0, 0, N);
+
+    printf("%d\n%d", result[0], result[1]);
+
+    return 0;
+}
